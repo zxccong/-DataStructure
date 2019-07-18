834099cfabe87b77662234c980e37a7ab7ce5473
zxccong <1209252967@qq.com>
2019-07-18 16:47:37 +0800

    简单实现int类型数组，增加、插入、删除、获取大小，是非为空等等等等

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -1,6 +1,7 @@
 public class Array {
     private int[] data;
 
+    //指向最后一个数组为空的索引
     private int size;
 
     /**
@@ -43,12 +44,165 @@ public class Array {
         return size==0;
     }
 
+    /**
+     * 添加元素到末尾
+     * @param e
+     */
     public void addLast(int e){
+//        if (size == data.length){
+//            throw new IllegalArgumentException("AddLast failed . Array is full");
+//
+//        }
+//        data[size] = e;
+//        size++;
+
+        //重用size
+        add(size,e);
+
+    }
+
+    /**
+     * 添加元素到数组前面
+     * @param e
+     */
+    public void addFirst(int e){
+        add(0,e);
+    }
+
+    /**
+     * 添加元素到自定未知
+     * @param index
+     * @param e
+     */
+    public void add(int index,int e){
         if (size == data.length){
-            throw new IllegalArgumentException("AddLast failed . Array is full");
+            throw new IllegalArgumentException("Add failed . Array is full");
 
         }
-        data[size] = e;
+        if (index<0||index>size){
+            throw new IllegalArgumentException("Add failed . Require index<0||index>size");
+        }
+
+        for (int i = size -1;i>=index;i--){
+            data[i+1] = data[i];
+        }
+        data[index]=e;
         size++;
     }
+
+    /**
+     * 通过index索引位置的元素为e
+     * @param index
+     * @return
+     */
+    int get(int index){
+        if (index<0||index>=size){
+            throw new IllegalArgumentException("Get Failed Index is illegal");
+        }
+        return data[index];
+    }
+
+    /**\
+     * 查找数组中是否有元素e
+     * @param e
+     * @return
+     */
+    public boolean contain(int e ){
+        for (int i = 0; i < size; i++) {
+            if (data[i]==e){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    /**
+     * 查找数组中元素e所在的位置，如果不存在元素e,则返回-1
+     * 只能查找一次可设计：查找出所有的位置
+     * @param e
+     * @return
+     */
+    public int find(int e ){
+        for (int i = 0; i < size; i++) {
+            if (data[i]==e){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 从数组中删除index位置的 元素，返回删除的元素
+     * @param index
+     * @return
+     */
+    public int remove(int index){
+        if (index<0||index>=size){
+            throw new IllegalArgumentException("Remove Failed Index is illegal");
+        }
+
+        int ret = data[index];
+        for (int i = index +1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        return ret;
+    }
+
+    /**
+     * 删除第一个元素
+     * @return
+     */
+    public int removeFirst(){
+        return remove(0);
+    }
+
+    /**
+     * 删除最后一个元素
+     * @return
+     */
+    public int removeLast(){
+        return remove(size-1);
+    }
+
+    /**
+     *  删除指定的元素值一次 （可设计：删除所有，删除后返回值）
+     * @param e
+     */
+    public void removeElement(int e){
+        int index = find(e);
+        if (index != -1){
+            remove(index);
+        }
+    }
+
+    /**
+     * 根据index索引位置设置元素e
+     * @param index
+     * @param e
+     */
+    void set(int index , int e ){
+        if (index<0||index>=size){
+            throw new IllegalArgumentException("Set Failed Index is illegal");
+        }
+        data[index] = e ;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d \n", size, data.length));
+        res.append('[');
+        for (int i = 0 ;i <size ; i++){
+            res.append(data[i]);
+            if (i != size-1){
+                res.append(",");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+
 }
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -12,14 +12,42 @@ public class Main {
 //            scores[0] = 98 ;
 //
 //        }
-//        String idCard= "341221199501122034";
-        String idCard= "341221199501122034";
-        if (Integer.parseInt(idCard.substring(16,17))%2==1){
-            System.out.println("男");
-        }else {
-            System.out.println("女");
-        }
 
+
+//        String idCard= "341221199501122034";
+//        String idCard= "341221199501122034";
+//        if (Integer.parseInt(idCard.substring(16,17))%2==1){
+//            System.out.println("男");
+//        }else {
+//            System.out.println("女");
+//        }
+
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; i++) {
+            arr.addLast(i);
+        }
+        System.out.println("arr = " + arr);
+
+        arr.add(1,100);
+        System.out.println("arr = " + arr);
+
+        arr.addFirst(500);
+        System.out.println("arr = " + arr);
+
+        arr.addLast(600);
+        System.out.println("arr = " + arr);
+
+        arr.remove(2);
+        System.out.println("arr = " + arr);
+
+        arr.removeElement(5);
+        System.out.println("arr = " + arr);
+
+        arr.removeFirst();
+        System.out.println("arr = " + arr);
+
+        arr.removeLast();
+        System.out.println("arr = " + arr);
 
     }
 }
